6815b39695525848b192b23aed5f1d4d8b449164
Monika <Monikasharma26081991@gmail.com>
2020-03-04 00:56:01 -0500

    Initial Commit

---
+++ C0767722_MidTerm_MAD3115W2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0767722_MidTerm_MAD3115W2020
+//
+//  Created by S@i on 2020-03-04.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

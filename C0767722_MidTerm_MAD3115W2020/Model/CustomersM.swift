95fce734d37f06a886b06be3e53677df2d1fc47c
Monika <Monikasharma26081991@gmail.com>
2020-03-08 17:46:00 -0400

    [Monika] Customer Model Added

---
+++ C0767722_MidTerm_MAD3115W2020/Model/CustomersM.swift
@@ -0,0 +1,16 @@
+//
+//  CustomersM.swift
+//  C0767722_MidTerm_MAD3115W2020
+//
+//  Created by S@i on 2020-03-07.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import Foundation
+
+struct CustomersM: Codable {
+    
+   
+    
+    
+}

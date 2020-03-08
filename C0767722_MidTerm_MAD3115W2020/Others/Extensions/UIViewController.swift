70da739dfb8d98c99bc2485c110dd65dca502ff0
Monika <Monikasharma26081991@gmail.com>
2020-03-08 17:50:03 -0400

    [Monika] Extension method added

--- C0767722_MidTerm_MAD3115W2020/Others/Extensions/UIViewController.swift
+++ C0767722_MidTerm_MAD3115W2020/Others/Extensions/UIViewController.swift
@@ -13,7 +13,22 @@ import UIKit
 extension UIViewController {
 
     ///
-   
+    func showAlert(title: String, message: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    ///
+    func navigateScreen(storyboard: String, controller: String) {
+        
+        let storyboard = UIStoryboard(name: storyboard, bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: controller)
+        
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 
 
 }

5565916f21d5180e7711558d16382c55a16ca14d
Monika <Monikasharma26081991@gmail.com>
2020-03-08 17:48:12 -0400

    [Monika] Uiview extension method added

---
+++ C0767722_MidTerm_MAD3115W2020/Others/Extensions/UIViewExtension.swift
@@ -0,0 +1,42 @@
+//
+//  UIViewExtension..swift
+//  C0767722_MidTerm_MAD3115W2020
+//
+//  Created by S@i on 2020-03-07.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UIView{
+    
+    func pathCurvedForView(givenView: UIView, curvedPercent:CGFloat) ->UIBezierPath
+    {
+        let arrowPath = UIBezierPath()
+        arrowPath.move(to: CGPoint(x:0, y:0))
+        arrowPath.addLine(to: CGPoint(x:givenView.bounds.size.width, y:0))
+        arrowPath.addLine(to: CGPoint(x:givenView.bounds.size.width, y:givenView.bounds.size.height - (givenView.bounds.size.height*curvedPercent)))
+        arrowPath.addQuadCurve(to: CGPoint(x:0, y:givenView.bounds.size.height - (givenView.bounds.size.height*curvedPercent)), controlPoint: CGPoint(x:givenView.bounds.size.width/2, y:givenView.bounds.size.height))
+        arrowPath.addLine(to: CGPoint(x:0, y:0))
+        arrowPath.close()
+        
+        return arrowPath
+    }
+    
+    func applyCurvedPath(givenView: UIView,curvedPercent:CGFloat) {
+        guard curvedPercent <= 1 && curvedPercent >= 0 else{
+            return
+        }
+        
+        let shapeLayer = CAShapeLayer(layer: givenView.layer)
+        shapeLayer.path = self.pathCurvedForView(givenView: givenView,curvedPercent: curvedPercent).cgPath
+        shapeLayer.frame = givenView.bounds
+        shapeLayer.masksToBounds = true
+        givenView.layer.mask = shapeLayer
+    }
+
+    
+      
+}
+

58872615ad1bef6f06c166c1668e4812fcbdfba5
Monika <Monikasharma26081991@gmail.com>
2020-03-08 17:43:07 -0400

    [Monika] Customer Class Added

---
+++ C0767722_MidTerm_MAD3115W2020/Model/CustomersVM.swift
@@ -0,0 +1,14 @@
+//
+//  CustomersVM.swift
+//  C0767722_MidTerm_MAD3115W2020
+//
+//  Created by S@i on 2020-03-07.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class CustomersVM: NSObject {
+
+}

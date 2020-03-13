68dec63f838dfbd674c6f95ce213b47b4aca2bf5
Monika <Monikasharma26081991@gmail.com>
2020-03-13 11:27:47 -0400

    [Monika] AddBill Vc added

---
+++ C0767722_MidTerm_MAD3115W2020/ViewController/AddNewBillVC.swift
@@ -0,0 +1,16 @@
+//
+//  AddNewBillVC.swift
+//  C0767722_MidTerm_MAD3115W2020
+//
+//  Created by S@i on 2020-03-13.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// this is the class to create new bill
+class AddNewBillVC: UIViewController {
+
+    
+

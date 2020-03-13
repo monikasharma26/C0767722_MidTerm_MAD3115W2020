1f20020323c6f6c815b26de5bc64f5eb21637539
Monika <Monikasharma26081991@gmail.com>
2020-03-13 11:25:37 -0400

    [Monika] Float Currency Method added

--- C0767722_MidTerm_MAD3115W2020/Others/Extensions/FloatExtension.swift
+++ C0767722_MidTerm_MAD3115W2020/Others/Extensions/FloatExtension.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+//Mark: - Link I used /
+// https://stackoverflow.com/questions/41558832/how-to-format-a-double-into-currency-swift-3
+extension Float {
+    func currency() -> String
+    {
+        let formatter = NumberFormatter()
+        formatter.locale = Locale(identifier: "en_US")
+        formatter.numberStyle = .currency
+        let formatmount = formatter.string(from: self as NSNumber)
+        let result = String(format: "%@", formatmount!)
+        return result
+    }
+    
+    // used to set as Units Usage.
+    func unitsUsage() -> String
+    {
+        let result = String(format: "%.2f MW", self)
+        return result
+    }
+}
+

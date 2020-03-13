4683bbeebe9906461655f3b8684ea01656a96d48
Monika <Monikasharma26081991@gmail.com>
2020-03-13 12:12:18 -0400

    [Monika] CustomerTableViewCell added

---
+++ C0767722_MidTerm_MAD3115W2020/ViewController/CustomerDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomerDetailTableViewCell.swift
+//  C0767722_MidTerm_MAD3115W2020
+//
+//  Created by S@i on 2020-03-13.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+
+class CustomerDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

578dc3384702bef675b526b6d2abf252b74748d3
Monika <Monikasharma26081991@gmail.com>
2020-03-13 14:03:44 -0400

    [Monika] Int Extension Added

---
+++ C0767722_MidTerm_MAD3115W2020/Others/Extensions/Int+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Int+Extension.swift
+//  C0767722_MidTerm_MAD3115W2020
+//
+//  Created by S@i on 2020-03-13.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import Foundation
+extension Int {
+    
+    var billWithGB: String {
+        return "\(self) GB"
+    }
+    
+    var billWithMinutes: String {
+        return "\(self) Minutes"
+    }
+    
+    var billWithUnits: String {
+        return "\(self) Units"
+    }
+}

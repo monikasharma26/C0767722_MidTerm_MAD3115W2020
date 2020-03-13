e9fd4047e6891fee4f8c59090da362b3fe8df859
Monika <Monikasharma26081991@gmail.com>
2020-03-13 14:02:15 -0400

    [Monika] Mobile class objects added

--- C0767722_MidTerm_MAD3115W2020/CustomersDetails/Bills/Mobile.swift
+++ C0767722_MidTerm_MAD3115W2020/CustomersDetails/Bills/Mobile.swift
@@ -8,5 +8,19 @@
 
 import Foundation
 class Mobile: Bill{
-    
+    internal var manufacturerName: String?
+       internal var planName: String?
+       internal var mobileNumber: String?
+       private var internetGBUsed: Int?
+       private var minutesUsed: Int?
+       
+       init(billId: Int?, billDate: String?, billType: BillType?, billAmount: Float?, manufacturerName: String?, planName: String?, mobileNumber: String?, internetGBUsed: Int?, minutesUsed: Int?) {
+           super.init(billId: billId, billDate: billDate, billType: billType, billAmount: billAmount)
+           self.manufacturerName = manufacturerName
+           self.planName = planName
+           self.mobileNumber = mobileNumber
+           self.internetGBUsed = internetGBUsed
+           self.minutesUsed = minutesUsed
+       }
+       
 }
